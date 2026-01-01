95cd27ce899e0f4beaf1ea1ab967e50916121285
213649163 <reemabedalrhman17@gmail.com>
2026-01-01 02:11:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ y = df["status"]
 
 # Train-test split (keep class balance)
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
+    X,
+    y,
     test_size=0.2,
     random_state=42,
     stratify=y
@@ -28,7 +29,7 @@ model = Pipeline([
     ("classifier", DecisionTreeClassifier(random_state=42))
 ])
 
-# Train
+# Train model
 model.fit(X_train, y_train)
 
 # Evaluate
